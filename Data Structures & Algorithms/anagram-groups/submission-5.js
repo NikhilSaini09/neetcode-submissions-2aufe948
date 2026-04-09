d261ab17518861f39719b63894742b26ac13a0fd
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-09 17:35:48 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (let str of strs){
+            let key = str.split('').sort().join('');
+            if (!map.has(key)){
+                map.set(key, []);
+            }
+            map.get(key).push(str);
+        }
+        return Array.from(map.values());
+
+
+
+        // const groups = {};
+
+        // for(const s of strs) {
+        //     const key = s.split('').sort().join('');
+        //     if(!groups[key]) groups[key] = [];
+        //     groups[key].push(s);
+        // }
+
+        // return Object.values(groups);
+
+
+        // const ans = [];
+        // let i = 0;
+        // const imap = new Map();
+        // for(const s of strs) {
+        //     const temp = s.split('').sort().join('');
+
+        //     if(imap.has(temp)) ans[imap.get(temp)].push(s);
+        //     else {
+        //         imap.set(temp, i++);
+        //         ans.push([s]);
+        //     }
+        // }
+
+        // return ans;
+    }
+}
