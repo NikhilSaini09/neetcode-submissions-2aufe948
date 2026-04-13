f09421d393ca971785d7d54bdfcd26bbf1de5d60
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-13 16:22:41 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int i = 0, j = n - 1;
+
+        while(true) {
+            while(i < n && i < j && !isalnum(s[i])) ++i;
+            while(j >= 0 && i < j && !isalnum(s[j])) --j;
+
+            if(i >= j) break;
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            ++i;
+            --j;
+        }
+
+        return true;
+    }
+};
