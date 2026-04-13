dc2e86e38b3e60d0e0b1943ccdb6d59892acd1d3
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-13 16:30:05 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        // const n = s.length;
+        // let i = 0, j = n - 1;
+        // const isAln = (char) => {
+        //     return /[A-Za-z0-9]/.test(char);
+        // };
+
+        // while(true) {
+        //     while(i < n && i < j && !isAln(s[i])) ++i;
+        //     while(j >= 0 && i < j && !isAln(s[j])) --j;
+
+        //     if(i >= j) break;
+        //     if(s[i++].toLowerCase() !== s[j--].toLowerCase()) return false;
+        // }
+
+        // return true;
+
+
+        const clean = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        return clean === clean.split('').reverse().join('');
+    }
+}
