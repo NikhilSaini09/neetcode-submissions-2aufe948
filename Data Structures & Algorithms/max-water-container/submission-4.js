10885061535f0bda48abf8fae3a2881aed7922fc
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-16 19:40:30 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let i = 0, j = heights.length - 1;
+        let ans = 0;
+
+        // while(i < j) {
+        //     if(heights[i] < heights[j]) {
+        //         ans = Math.max(ans, heights[i] * (j - i));
+        //         ++i;
+        //     } else {
+        //         ans = Math.max(ans, heights[j] * (j - i));
+        //         --j;
+        //     }
+        // }
+
+        while(i < j) {
+            const LH = Math.min(heights[i], heights[j]);
+            ans = Math.max(ans, LH * (j - i));
+            if(heights[i] == LH) ++i;
+            else --j;
+        }
+
+        return ans;
+    }
+}
