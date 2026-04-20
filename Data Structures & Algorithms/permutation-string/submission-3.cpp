975ef97f2634f1f3857cf558fd0e43d3ab9e3115
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-20 15:55:54 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int m = s1.length(), n = s2.length();
+        if(m > n) return false; 
+        int hash[26] = {0};
+
+        for(char c : s1) hash[c - 'a']++;
+
+        int l = 0, r = 0;
+        while(r < n) {
+            if(hash[s2[r] - 'a'] > 0) {
+                --hash[s2[r] - 'a'];
+            } else {
+                if(l < r) while(s2[r] != s2[l]) ++hash[s2[l++] - 'a'];
+                ++l;
+            }
+            
+            if(r - l + 1 == m) return true;
+            ++r;
+        }
+
+        return false;
+    }
+};
