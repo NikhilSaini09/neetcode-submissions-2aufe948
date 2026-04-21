27102a9ee5af477367f4cfa89cf95784d60f2cef
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-04-21 15:49:25 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        const n = nums.length;
+        const ans = [];
+        const dq = [];
+
+        for(let i = 0; i < n; ++i) {
+            if(dq.length > 0 && dq[0] == i - k) dq.shift();
+            // while(dq.length > 0 && nums[dq[dq.length - 1]] <= nums[i]) dq.pop();
+            while(dq.length > 0 && nums[dq.at(-1)] <= nums[i]) dq.pop();
+
+            dq.push(i);
+            if(i >= k - 1) ans.push(nums[dq[0]]);
+        }
+
+        return ans;
+    }
+}
