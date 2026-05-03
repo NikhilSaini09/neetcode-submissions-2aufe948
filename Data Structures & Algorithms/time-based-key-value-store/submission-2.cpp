18c6fbdce9e32ffb30cbdfa1d3301b4f14690b89
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-05-03 16:17:23 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,46 @@
+// class TimeMap {
+// private:
+//     unordered_map<string, set<pair<int, string>>> keys;
+// public:
+//     TimeMap() {}
+    
+//     void set(string key, string value, int timestamp) {
+//         keys[key].insert({timestamp, value});
+//     }
+    
+//     string get(string key, int timestamp) {
+//         if(!keys.count(key)) return "";
+//         auto it = keys[key].upper_bound({timestamp, "\xff"});
+//         if(it == keys[key].begin()) return "";
+
+//         it = std::prev(it);
+//         return it->second;
+//     }
+// };
+
+
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> keys;
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        keys[key].emplace_back(timestamp, value);
+    }
+    
+    string get(string key, int timestamp) {
+        auto it2 = keys.find(key);
+        if(it2 == keys.end()) return "";
+
+        auto &v = it2->second;
+        // auto it = upper_bound(v.begin(), v.end(), make_pair(timestamp, string(1, 127)));
+        auto it = upper_bound(v.begin(), v.end(), make_pair(timestamp, ""), 
+            [] (const pair<int, string>& target, const pair<int, string>& element) {
+                return target.first < element.first; 
+            });
+        if(it == v.begin()) return "";
+
+        return prev(it)->second;
+    }
+};
