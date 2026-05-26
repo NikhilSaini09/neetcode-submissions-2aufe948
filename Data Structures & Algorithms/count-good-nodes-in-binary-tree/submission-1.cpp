4c4b243ac7207916ce2cf01ea1ce27512564c7d5
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-05-26 20:53:49 +0530

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// class Solution {
+//     void dfs(TreeNode* node, int &cnt, int pmax) {
+//         if(!node) return;
+//         if(node->val >= pmax) {
+//             cnt++;
+//             pmax = node->val;
+//         }
+
+//         dfs(node->left, cnt, pmax);
+//         dfs(node->right, cnt, pmax);
+//     }
+// public:
+//     int goodNodes(TreeNode* root) {
+//         ios::sync_with_stdio(false);
+//         cin.tie(NULL);
+//         cout.tie(NULL);
+
+//         int cnt = 0;
+//         dfs(root, cnt, -1e5);
+//         return cnt;
+//     }
+// };
+
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return dfs(root, -150);
+    }
+    int dfs(TreeNode* root, int high) {
+        if(!root) return 0;
+
+        int cur = root->val >= high ? 1 : 0;
+
+        high = max(high, root->val);
+        return cur + dfs(root->left, high) + dfs(root->right, high);
+    }
+};
