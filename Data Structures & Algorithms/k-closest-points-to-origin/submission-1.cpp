0a2ef9622111a8fb004d815bb41ffe2a7dbc27a3
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-06-03 18:40:35 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        priority_queue<pair<int, int>> pq;
+
+        for(int i = 0; i < points.size(); ++i) {
+            int distSq = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+
+            pq.emplace(distSq, i);
+            if(pq.size() > k) pq.pop();
+        }
+
+        vector<vector<int>> ans;
+        ans.reserve(k);
+        while(!pq.empty()) {
+            ans.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         priority_queue<pair<double, pair<int, int>>> pq;
+
+//         for(const auto &p : points) {
+//             double dist = sqrt(p[0] * p[0] + p[1] * p[1]);
+//             pq.push({dist, {p[0], p[1]}});
+//             if(pq.size() > k) pq.pop();
+//         }
+
+//         vector<vector<int>> ans;
+//         ans.reserve(k);
+//         while(!pq.empty()) {
+//             auto [i, j] = pq.top().second;
+//             ans.push_back({i, j});
+//             pq.pop();
+//         }
+
+//         return ans;
+//     }
+// };
