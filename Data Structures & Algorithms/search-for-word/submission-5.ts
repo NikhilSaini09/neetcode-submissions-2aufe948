b1a1441cb6d79b8017ac406d25e3d0e48738d466
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-06-14 20:26:05 +0530

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board: string[][], word: string): boolean {
+        const m: number = board.length;
+        const n: number = board[0].length;
+
+        const dfs = (r: number, c: number, idx: number): boolean => {
+            if(idx === word.length) return true;
+            if(r < 0 || c < 0 || r >= m || c >= n) return false;
+            if(board[r][c] !== word[idx]) return false;
+
+            const temp = board[r][c];
+            board[r][c] = '#';
+
+            const found = dfs(r + 1, c, idx + 1) ||
+                        dfs(r - 1, c, idx + 1) ||
+                        dfs(r, c + 1, idx + 1) ||
+                        dfs(r, c - 1, idx + 1);
+
+            board[r][c] = temp;
+            
+            return found;
+        };
+
+        for(let i = 0; i < m; i++) {
+            for(let j = 0; j < n; j++) {
+                if(dfs(i, j, 0)) return true;
+            }
+        }
+
+        return false;
+    }
+}
