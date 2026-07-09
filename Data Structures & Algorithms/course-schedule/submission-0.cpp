128ef5aa84c6f9368a0a386c8c7fef4c7ad05831
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-09 14:41:30 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for(auto &p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; ++i) {
+            if(indegree[i] == 0) {
+                q.push(i);;
+            }
+        }
+
+        int cnt = 0;
+        while(!q.empty()) {
+            int u = q.front(); q.pop();
+            cnt++;
+
+            for(int v : adj[u]) {
+                if(--indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        return cnt == numCourses;
+    }
+};
