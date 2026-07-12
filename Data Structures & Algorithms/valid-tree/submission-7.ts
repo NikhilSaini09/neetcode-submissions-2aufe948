060f79520c32e7dfa2c08b6502283cbad59b5f02
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-12 12:23:01 +0530

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n: number, edges: number[][]): boolean {
+        if(edges.length != n - 1) return false;
+
+        const adj: number[][] = Array.from({ length: n }, () => []);
+        for(const [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        const vis: boolean[] = new Array(n).fill(false);
+        vis[0] = true;
+
+        const dfs = (u: number): void => {
+            for(const v of adj[u]) {
+                if(!vis[v]) {
+                    vis[v] = true;
+                    dfs(v);
+                }
+            }
+        }
+
+        dfs(0);
+
+        // for(const b of vis) if(!b) return b;
+        // return true;
+        // return vis.every(Boolean);
+        return !vis.includes(false);
+    }
+}
