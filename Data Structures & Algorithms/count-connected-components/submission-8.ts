f63a657dd7e0577ca8b4ab04bf258bc3d6b3d53a
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-13 12:10:36 +0530

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n: number, edges: number[][]): number {
+        const adj: number[][] = Array.from({ length: n }, () => []);
+        for(const [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        const vis: boolean[] = new Array(n).fill(false);
+
+        const dfs = (u: number): void => {
+            vis[u] = true;
+            for(const v of adj[u]) {
+                if(!vis[v]) dfs(v);
+            }
+        };
+
+        let ans: number = 0;
+        for(let i = 0; i < n; ++i) {
+            if(!vis[i]) {
+                ans++;
+                dfs(i);
+            }
+        }
+        return ans;
+    }
+}
