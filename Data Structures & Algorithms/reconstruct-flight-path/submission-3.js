153e5f19351c104803b84014c189c60206ea2dd0
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-18 00:08:57 +0530

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[][]} tickets
+     * @return {string[]}
+     */
+    findItinerary(tickets) {
+        const graph = {};
+    
+        for (const [src, dst] of tickets) {
+            if (!graph[src]) graph[src] = [];
+            graph[src].push(dst);
+        }
+        
+        for (const key in graph) {
+            graph[key].sort().reverse();
+        }
+        
+        const itinerary = [];
+        
+        function dfs(airport) {
+            while (graph[airport] && graph[airport].length > 0) {
+                dfs(graph[airport].pop());
+            }
+            itinerary.push(airport);
+        }
+        
+        dfs("JFK");
+        
+        return itinerary.reverse();
+    }
+}
