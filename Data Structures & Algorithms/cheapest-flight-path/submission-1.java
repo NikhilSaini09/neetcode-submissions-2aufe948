a6295b5321f9597128a3ce3869417a315acb042e
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-23 15:05:07 +0530

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; ++i) adj.add(new ArrayList<>());
+
+        for(int[] f : flights) {
+            adj.get(f[0]).add(new int[]{f[1], f[2]});
+        }
+
+        int[] cost = new int[n];
+        Arrays.fill(cost, (int)1e9);
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{src, 0});
+        cost[src] = 0;
+
+        while(!q.isEmpty()) {
+            int qs = q.size();
+
+            while(qs-- > 0) {
+                int[] cur = q.poll();
+
+                for(int[] v : adj.get(cur[0])) {
+                    if(cost[v[0]] > cur[1] + v[1]) {
+                        cost[v[0]] = cur[1] + v[1];
+                        q.offer(new int[]{v[0], cost[v[0]]});
+                    }
+                }
+            }
+
+            if(k-- == 0) break;
+        }
+
+        return cost[dst] == 1e9 ? -1 : cost[dst];
+    }
+}
