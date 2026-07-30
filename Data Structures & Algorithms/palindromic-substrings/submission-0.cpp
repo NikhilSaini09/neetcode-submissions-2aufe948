0415e6acde4e12f2d4d28e5475e6f4f188e54484
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-07-30 18:33:10 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+        
+        auto expand = [&](int l, int r) {
+            while(l >= 0 && r < n && s[l] == s[r]) {
+                ans++;
+                l--, r++;
+            }
+        };
+
+        for(int i = 0; i < n; ++i) {
+            expand(i, i);
+            expand(i, i + 1);
+        }
+
+        return ans;
+    }
+};
