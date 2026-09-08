b099d8007aeb65830185dade30bd32e3cca62143
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-09-08 23:53:15 +0530

    Add: merge-triplets-to-form-target - submission-14

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-14.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[][]} triplets
+     * @param {number[]} target
+     * @return {boolean}
+     */
+    mergeTriplets(triplets: number[][], target: number[]): boolean {
+        let ans: number = 0;
+        for(const [a, b, c] of triplets) {
+            if(a <= target[0] && b <= target[1] && c <= target[2]) {
+                if(a === target[0]) ans |= 1;
+                if(b === target[1]) ans |= 2;
+                if(c === target[2]) ans |= 4;
+            }
+        }
+        return ans === 7;
+    }
+}
