0730e8a4991add27e8f15792f172a1ca3343bf11
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-09-17 23:58:14 +0530

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    checkValidString(s) {
+        let mini = 0;
+        let maxi = 0;
+        for(const c of s) {
+            if(c == '(') mini++, maxi++;
+            else if(c == ')') mini--, maxi--;
+            else mini--, maxi++;
+
+            if(maxi < 0) return false;
+            if(mini < 0) mini = 0;
+        }
+        return mini == 0;
+    }
+}
