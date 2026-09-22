baab27482fb838e586bbdec05f9c9b149009dfcc
Nikhil Saini <134204015+NikhilSaini09@users.noreply.github.com>
2026-09-22 21:11:18 +0530

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+        const ans = [];
+        for(const [st, ed] of intervals) {
+            if(ans.length === 0 || ans.at(-1)[1] < st) ans.push([st, ed]);
+            else ans[ans.length - 1][1] = Math.max(ans.at(-1)[1], ed);
+        }
+        return ans;
+    }
+}
